8c10fd96812a7b0495083d21fb4a2fe446bc9043
Victorpraise5 <victorpraise949@gmail.com>
2024-11-27 17:13:24 +0300

    minor changes

--- Flask_blog/flaskblog.py
+++ Flask_blog/flaskblog.py
@@ -37,9 +37,15 @@ def register():
     return redirect(url_for('home'))
   return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
   form = LoginFrom()
+  if form.validate_on_submit():
+    if form.email.data == 'stacynjoki@gmail.com' and form.password.data == 'njoki':
+      flash('You have been logged in!', 'success')
+      return redirect(url_for('home'))
+    else:
+      flash('Login unsuccessful. Please check username and password', 'danger')
   return render_template('login.html', title='login', form=form)
 
 if __name__ == '__main__':
